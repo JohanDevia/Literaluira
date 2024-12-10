b2345eb2e0367d6ffe76d68641b4ef0562ffedbe
JohanDevia <johandeviag@gmail.com>
2024-12-09 19:11:39 -0500

    funcion de buscar libros por idioma añadida

--- literalura/src/main/java/com/alura/literalura/service/LibroService.java
+++ literalura/src/main/java/com/alura/literalura/service/LibroService.java
@@ -13,31 +13,14 @@ public class LibroService {
 	@Autowired
 	private LibroRepository libroRepository;
 
-	/**
-	 * Obtiene todos los libros registrados en la base de datos.
-	 *
-	 * @return Lista de libros.
-	 */
 	public List<Libro> obtenerTodosLosLibros() {
 		return libroRepository.findAll();
 	}
 
-	/**
-	 * Obtiene libros filtrados por idioma.
-	 *
-	 * @param idioma Idioma en formato ISO (por ejemplo, "en", "es").
-	 * @return Lista de libros en el idioma especificado.
-	 */
 	public List<Libro> obtenerLibrosPorIdioma(String idioma) {
 		return libroRepository.findByIdioma(idioma);
 	}
 
-	/**
-	 * Guarda un libro en la base de datos.
-	 *
-	 * @param libro Objeto Libro a guardar.
-	 * @return El libro guardado.
-	 */
 	public Libro guardarLibro(Libro libro) {
 		return libroRepository.save(libro);
 	}
